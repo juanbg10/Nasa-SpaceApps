2b7af23644debd1b39a8579167fd10e90a802075
Ingrid Bianca <ingridbianca10@gmail.com>
2019-10-20 15:52:52 -0300

    Ajustes no Initializer

--- script banco/table nasarenos.sql
+++ script banco/table nasarenos.sql
@@ -1,7 +1,7 @@
 create database nasarenos;
 
-create table login (cd_login int primary key not null, nm_email varchar(50) not null , ds_senha varchar(20) not null, cd_usuario int foreign key);
-create table usuario (cd_usuario int primary key not null, nm_usuario varchar(25), ds_cep varchar(9), dt_nascimento date, cd_tipo int, ds_moradia varchar(5), nm_instituicao varchar(30), ds_comprovante varchar(30));
+create table usuario (cd_usuario int primary key, nm_usuario varchar(25), ds_cep varchar(9), dt_nascimento date, cd_tipo int, ds_moradia varchar(5), nm_instituicao varchar(30), ds_comprovante varchar(30));
+create table login (cd_login int primary key, nm_email varchar(50) not null , ds_senha varchar(20) not null, cd_usuario int);
 create table local (cd_local int primary key, ds_lat varchar(20), ds_long varchar(10), ds_temperatura varchar(5), dt_local date, dt_repar date, ds_vegetacao varchar(20), ds_plurometrico varchar(20));
 create table criterio (cd_criterio int primary key, nm_criterio varchar(25));	
 
@@ -14,9 +14,6 @@ insert into nasarenos.criterio(1,"temperatura");
 insert into criterio(2,"vegetacao");
 insert into criterio(3,"plurometrico");
 
-insert into local {1, "72.18", "-121.82," "25", "2010-02-26", "20-10-2010", null, "0.02"};
-insert into local {2, "80.99", "162.51", "20", "2010-02-26", "20-10-2010", null, null};
-
-
-
+insert into local (1, "72.18", "-121.82," "25", "2010-02-26", "20-10-2010", null, "0.02");
+insert into local (2, "80.99", "162.51", "20", "2010-02-26", "20-10-2010", null, null);
 
